d650208b554f185115a0f927f32ddd276672f84f
Felipe Mosquera <felipe298@gmail.com>
2024-05-16 08:03:50 +0200

    Añadir tres inputs: nombre de presupuesto y cliente/a, que junto con el servicio seleccionado por el usuario/ay el precio total calculado,Ejercicio 5

--- src/Components/Budgets/Budgets.jsx
+++ src/Components/Budgets/Budgets.jsx
@@ -5,7 +5,13 @@ import { Section } from "../Section/Section";
 
 
 export const Budgets = () => {
-  const { shipping, total, handleShipping, incrementCount, decrementCount } =
+  const { shipping, total, handleShipping, incrementCount, decrementCount,clientEmail,
+    clientName,
+    clientTelephone,
+    estimate,addBudget,setClientName,
+    setclientEmail,
+    setClientTelephone
+} =
     useShipping();
 
   return (
@@ -80,7 +86,87 @@ export const Budgets = () => {
       <h2 className="text-2xl">
         <strong>{total}€</strong>
       </h2>
-      
+      <div className="p-4 max-w-lg mx-auto ">
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">
+            Nombre del cliente/a
+          </label>
+          <input
+            type="text"
+            value={clientName}
+            onChange={(e) => setClientName(e.target.value)}
+            placeholder="Introduce el nombre del cliente"
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">
+            Email del cliente/a
+          </label>
+          <input
+            type="text"
+            value={clientEmail}
+            onChange={(e) => setclientEmail(e.target.value)}
+            placeholder="Introduce el Email del cliente"
+            className="w-full p-2 border rounded"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium mb-1">
+            Telefono del cliente/a
+          </label>
+          <input
+            type="text"
+            value={clientTelephone}
+            onChange={(e) => setClientTelephone(e.target.value)}
+            placeholder="Introduce el telfono del cliente"
+            className="w-full p-2 border rounded"
+          />
+        </div>
+        <button
+          type="button"
+          onClick={addBudget}
+          className="mt-4 px-4 py-2 bg-blue-500 tex-white rounded"
+        >
+          Añadir Presupuesto
+        </button>
+      </div>
+      <div className="mb-3bg-white p-6 rounded-lg shadow-md mb-4 flex justify-between ">
+        {estimate.length > 0 && (
+          <div className="mt-8">
+            <h3 className="text-3xl font-bold">Demanar pressupost</h3>
+            <ul className="mt-4">
+              {estimate.map((budget, index) => (
+                <li key={index} className="">
+                  <strong>
+                    <label className="block text-sm font-medium mb-1">
+                      Nombre del cliente/a
+                    </label>
+                  </strong>
+                  <strong>{budget.clientName}</strong>  
+                  <strong>
+                    <label className="block text-sm font-medium mb-1">
+                      Email del cliente/a
+                    </label>
+                  </strong>
+                  <strong>{budget.clientEmail}{" "}</strong>
+                  <strong>
+                    <label className="block text-sm font-medium mb-1">
+                      Telefono del cliente/a
+                    </label>
+                  </strong>
+                  <strong>{budget.clientTelephone}</strong>
+                  <label className="block text-sm font-medium mb-1">
+                    Total:
+                  </label>
+                  <strong>{budget.total}€</strong>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- src/context/ShippingContext.jsx
+++ src/context/ShippingContext.jsx
@@ -1,75 +1,134 @@
-import { createContext,useContext,useState,useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
+const ShippingContext = createContext();
 
-const ShippingContext = createContext()
+export const ShippingProvider = ({ data, children }) => {
+  const [shipping, setShipping] = useState(
+    data.reduce(
+      (acc, item) => ({
+        ...acc,
+        [item.id]: {
+          checked: false,
+          subItems: item.subItems.reduce(
+            (subAcc, subItem) => ({
+              ...subAcc,
+              [subItem.id]: { count: 1, value: subItem.base },
+            }),
+            {}
+          ),
+        },
+      }),
+      {}
+    )
+  );
+  const [total, setTotal] = useState(0);
+  const [clientEmail, setclientEmail] = useState("");
+  const [clientName, setClientName] = useState("");
+  const [clientTelephone, setClientTelephone] = useState("");
+  const [estimate, setEstimate] = useState([]);
 
+  useEffect(() => {
+    const newTotal = data.reduce((acc, item) => {
+      const itemTotal = shipping[item.id].checked ? item.price : 0;
+      const subItemsTotal = Object.values(shipping[item.id].subItems).reduce(
+        (subAcc, subItem) => {
+          return (
+            subAcc +
+            (shipping[item.id].checked ? subItem.count * subItem.value : 0)
+          );
+        },
+        0
+      );
 
+      return acc + itemTotal + subItemsTotal;
+    }, 0);
+    setTotal(newTotal);
+  }, [shipping, data]);
 
-export const ShippingProvider = ({data,children}) => {
-    const [shipping, setShipping] = useState(
-        data.reduce(
-          (acc, item) => ({
-            ...acc,
-            [item.id]: {
-              checked: false,
-              subItems: item.subItems.reduce(
-                (subAcc, subItem) => ({
-                  ...subAcc,
-                  [subItem.id]: { count: 1, value: subItem.base }
-                }), {})}
-            
-          }),
-          {}
-        )
-      );  
-      const [total, setTotal] = useState(0);
-      useEffect(() => {
-        const newTotal = data.reduce((acc, item) => {
-          const itemTotal = shipping[item.id].checked ? item.price:0;
-          const subItemsTotal = Object.values(shipping[item.id].subItems).reduce((subAcc,subItem)=>{
-            return subAcc + (shipping[item.id].checked ? subItem.count * subItem.value : 0);
-          },0)
-         
-          return acc + itemTotal + subItemsTotal
-          
-        }, 0);
-        setTotal(newTotal);
-      }, [shipping,data]);
-
-
-
-      const handleShipping = (id,subId=null,value=null) => {
-        setShipping((prev) => ({
-          ...prev,
-          [id]: subId?{
+  const handleShipping = (id, subId = null, value = null) => {
+    setShipping((prev) => ({
+      ...prev,
+      [id]: subId
+        ? {
             ...prev[id],
-            subItems:{
+            subItems: {
               ...prev[id].subItems,
-              [subId]:{
+              [subId]: {
                 ...prev[id].subItems[subId],
-                count: value !== null ? value :
-                prev[id].subItems[subId].count + 1
-              }
-            }
-          } : {...prev[id], checked: !prev[id].checked}
-        }));
+                count:
+                  value !== null ? value : prev[id].subItems[subId].count + 1,
+              },
+            },
+          }
+        : { ...prev[id], checked: !prev[id].checked },
+    }));
+  };
+  const incrementCount = (id, subId) =>
+    handleShipping(id, subId, shipping[id].subItems[subId].count + 1);
+  const decrementCount = (id, subId) => {
+    const currentCount = shipping[id].subItems[subId].count;
+    if (currentCount > 0) handleShipping(id, subId, currentCount - 1);
+  };
 
-       
-      
-      }
-      const incrementCount = (id, subId) =>
-        handleShipping(id, subId, shipping[id].subItems[subId].count + 1);
-      const decrementCount = (id, subId) => {
-        const currentCount = shipping[id].subItems[subId].count;
-        if (currentCount > 0) handleShipping(id, subId, currentCount - 1);
-      };
-      return (
-        <ShippingContext.Provider value={{ shipping, total, handleShipping,incrementCount,decrementCount}}>
-        {children}
-      </ShippingContext.Provider>
-  )
+  const addBudget = () => {
+    setEstimate([
+      ...estimate,
+      {
+        clientName,
+        clientEmail,
+        clientTelephone,
+        total,
+      },
+    ]);
+    
+    setclientEmail("");
+    setClientName("");
+    setClientTelephone("");
+
+    setShipping(
+      data.reduce(
+        (acc, item) => ({
+          ...acc,
+          [item.id]: {
+            checked: false,
+            subItems: item.subItems.reduce(
+              (subAcc, subItem) => ({
+                ...subAcc,
+                [subItem.id]: { count: 1, value: subItem.base },
+              }),
+              {}
+            ),
+          },
+        }),
+        {}
+      )
+    );
+    setTotal(0);
+  };
+
+  return (
+    <ShippingContext.Provider
+      value={{
+        shipping,
+        total,
+        clientEmail,
+        clientName,
+        clientTelephone,
+        estimate,
+        handleShipping,
+        incrementCount,
+        decrementCount,
+        addBudget,
+        setClientName,
+        setclientEmail,
+        setClientTelephone
 
 
-}
+      }}
+    >
+      {children}
+    </ShippingContext.Provider>
+  );
+};
 
 export const useShipping = () => useContext(ShippingContext);
